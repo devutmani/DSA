e6323ae6330723e158bfa22aa2afee9e97583b56
Dev Kumar <devutmani.2@gmail.com>
2025-10-03 17:27:52 +0500

    Added permutation problem

--- DSA Theory/Recursion/permutation.cpp
+++ DSA Theory/Recursion/permutation.cpp
@@ -1,3 +1,22 @@
+/**
+* Name: Dev Kumar
+* Department: AI-3A
+* Problem: Permutaion
+* Date: 10/3/2025
+*                       <------------(123)------------>
+*                                      ^
+*                                      |
+*          (i = 0)                   _ _ _                 (idx = 0)
+*                                 /    |   \       
+*                                /     |    \      
+*          (i = 1)             1_._   2_._  3_._           (idx = 1)
+*                              / \     /\     / \      
+*                             /   \   /  \   /   \     
+*          (i = 2)           12_ 13_ 21_ 23_ 31_ 32_       (idx = 2)
+*                             |   |   |   |   |   |        
+*          (i = 3)           123 132 213 231 312 321       (idx = 3)
+*/
+
 #include <iostream>
 #include <vector>
 using namespace std;
