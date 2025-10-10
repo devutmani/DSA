e769d0e6531f6516a85690d37cfb392d3b4407d8
Dev Kumar <devutmani.2@gmail.com>
2025-10-10 22:45:27 +0500

    Updated random cpp file (pattern printing)

--- DSA Lab/random.cpp
+++ DSA Lab/random.cpp
@@ -1,7 +1,34 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main() {
-    cout << "Random File" << endl;
-    return 0;
-}
+// int main() {
+//     int n;
+
+//     cout << "\nEnter n: ";
+//     cin >> n;
+
+//     // --------------(Upper Triangle)--------------
+//     cout << "\nPattern: " << endl;
+//     for (int i = 0; i < (n/2)+1; i++) {
+//         for (int j = 0; j < (n/2)-i; j++) {
+//             cout << " ";
+//         }
+//         for (int k = 0; k < (2*i)+1; k++) {
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+
+//     //----------------(Lower Triangle)--------------
+//     for (int i = 0; i < (n/2); i++) {
+//         for (int j = 0; j < (2*i)+1-i; j++) {
+//             cout << " ";
+//         }
+
+//         for (int k = 0; k <= (n/2)-i; k++) {
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
